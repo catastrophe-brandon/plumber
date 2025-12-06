897d86f1ee0b1de8eec54ff04fb78ec73fb49690
Darth Tweed <btweed@redhat.com>
2025-12-06 16:17:47 -0600

    Add local helper scripts

---
+++ scripts/generate_learning_resources_pipeline.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# Run plumber for learning-resources
+echo "Generating pipeline for learning-resources..."
+plumber learning-resources \
+  https://github.com/RedHatInsights/learning-resources.git \
+  --pipeline-template template/konflux_pipeline_template.yaml \
+  --fec-config fec_configs/fec.config.js
+
+# Copy the generated file to the target location
+echo "Copying pipeline file to ~/repos/js/learning-resources/.tekton/..."
+cp learning-resources-pull-request.yaml ~/repos/js/learning-resources/.tekton/learning-resources-pull-request.yaml
+
+echo "✓ Pipeline file copied to ~/repos/js/learning-resources/.tekton/learning-resources-pull-request.yaml"
